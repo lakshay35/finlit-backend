a6bac9f851e21c0c9b7b8c3a3033992ee77bcf7f
Lakshay Sharma <lakshay35@gmail.com>
2021-06-30 16:22:02 -0400

    chore: commitlint now enforces custom scopes

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,21 @@
-module.exports = {extends: ['@commitlint/config-conventional']}
+module.exports = {extends: ['@commitlint/config-conventional'],
+rules: {
+  "type-enum": [
+    2,
+    "always",
+    [
+      "build",
+      "chore",
+      "ci",
+      "docs",
+      "feat",
+      "fix",
+      "perf",
+      "refactor",
+      "revert",
+      "style",
+      "test",
+    ],
+  ],
+  "scope-enum": [2, "always", ["fitness-tracker", "budgeter"]],
+},}
